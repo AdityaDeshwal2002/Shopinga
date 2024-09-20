37acf20a60476cdc654f42c7b2bf0466a3b4bd4d
AdityaDeshwal2002 <adityadeshwal02@gamil.com>
2024-09-20 23:49:20 +0530

    On the Way

--- app/src/main/java/com/asn8/shopinga/ShoppingItemAdapter.kt
+++ app/src/main/java/com/asn8/shopinga/ShoppingItemAdapter.kt
@@ -1,24 +1,29 @@
 package com.asn8.shopinga
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.asn8.shopinga.data.db.enities.ShoppingItem
 import com.asn8.shopinga.ui.shoppinglist.ShoppingViewModel
+import com.asn8.shopinga.R
 
 class ShoppingItemAdapter(var item : List<ShoppingItem>,private val viewModel: ShoppingViewModel)
     :RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
     inner class ShoppingViewHolder(item :View):RecyclerView.ViewHolder(item)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item,parent,false)
+        return ShoppingViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return item.size
     }
 
     override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val curShoppingItem = item[position]
+        
+
     }
 }
